db8934c407c8449c7af2fd13bb3b64e735c18da4
perebeynos.roma@gmail.com <perebeynos.roma@gmail.com>
2020-02-09 19:09:32 +0200

    враоп

---
+++ FootballStatisticsWeb2/Data/Models/Championship.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballStatisticsWeb2.Data.Models
+{
+    public class Championship
+    {
+        public int Id { get; set;  }
+        public string Name { get; set; }
+        public string CountryName { get; set; }
+        public int NumberOfTeams { get; set; }
+        public string Description { get; set; }
+
+        public List<Team> Teams { get; set; }
+
+        public List<TableOfChampionship> Results { get; set; }
+
+
+
+
+    }
+}
---
+++ FootballStatisticsWeb2/Data/Models/TableOfChampionship.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballStatisticsWeb2.Data.Models
+{
+    public class TableOfChampionship
+    {
+        public int Id { get; set; }
+        public string Season { get; set; }
+
+        public int champshipID { get; set; }
+        public Championship champship { get; set; }
+
+
+
+
+    }
+}
